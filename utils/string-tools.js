a355d804838f5371dfcd69a39cf74b89d03ad5e3
mengqingshen <mengqingshen_sean@outlook.com>
2017-12-09 01:28:06 +0800

    Fix: parse the folder name to a wrong format

--- utils/string-tools.js
+++ utils/string-tools.js
@@ -7,5 +7,5 @@ module.exports = {
   isFileName: fileName => /[a-z]+(-[a-z])*/.test(fileName),
   toFileName: name => name.replace(/([A-Z])/g, '-$1').toLowerCase(),
   isFolderName: folderName => /[a-z]+(_[a-z])*/.test(folderName),
-  toFolderName: name => name.replace(/([A-Z])/g, '_$1').toLowerCase()
+  toFolderName: name => name.replace(/([a-z])([A-Z])/g, '$1_$2').toLowerCase()
 }
