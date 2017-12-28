c876c26ee8d67edb4b1df80ed74bf22a862397fa
孟庆申 <mengqingshen@meituan.com>
2017-12-28 20:55:43 +0800

    test dol

--- bin/dol.js
+++ bin/dol.js
@@ -7,8 +7,26 @@
 
 const yeoman = require('yeoman-environment')
 
-console.log('argv', process.argv)
 const env = yeoman.createEnv()
 env.register(require.resolve('generator-dolphin'), 'dolphin:app')
 
-env.run('dolphin:app', () => {})
+const validCommands = new Map([
+  ['init', 'app'],
+  ['app', 'app'],
+  ['page', 'page'],
+  ['module', 'module'],
+  ['clean', 'clean']
+])
+
+const DEFAULT_COMMAND = 'init'
+
+const command = process.argv[2] || DEFAULT_COMMAND
+
+if (validCommands.has(command)) {
+  env.run(`dolphin:${validCommands.get(command)} ${process.argv.slice(3)}`, () => {})
+} else {
+  console.error('无效的指令！')
+  console.log(`
+    dol ${validCommands.keys().join('|')}
+  `)
+}
